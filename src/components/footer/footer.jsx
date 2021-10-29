c528a8d0f8cf9f280d36ee7839fca66d9d2bcc8f
Tulikasharma <tulikasharma368@gmail.com>
2021-10-29 10:07:41 +0530

    [Tulika]added: Footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./footer.scss";
+const Footer = () => {
+  return (
+    <div className="footerdiv">
+      <div className="footercontent">
+        <p>Cppyright © 2020, Bookstore Private Limited, All Rights Reserved</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../../components/header/Header";
 import Books from "../../components/books/Books";
+import Footer from "../../components/footer/footer";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Books />
+      <Footer />
     </div>
   );
 };
