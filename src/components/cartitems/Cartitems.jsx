57a7c0680e7b00aa1775a789ab107cb6a24646d1
Tulikasharma <tulikasharma368@gmail.com>
2021-10-30 11:32:13 +0530

    [Tulika]added: Cart UI static

--- src/components/cartitems/Cartitems.jsx
+++ src/components/cartitems/Cartitems.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import "./cartitems.scss";
 import book1 from "../../assets/books/book1.png";
+import TextField from "@mui/material/TextField";
 
 const Cartitems = () => {
   const [countervalue, setcountervalue] = React.useState(0);
   const [orderbutton, setorderbutton] = React.useState(true);
+  const [continuebutton, setcontinuebutton] = React.useState(true);
   const [custdetails, setcustdetails] = React.useState(false);
   const [ordersummary, setordersummary] = React.useState(false);
 
   const placeorder = () => {
     setorderbutton(false);
     setcustdetails(true);
+  };
+
+  const continueorder = () => {
+    setcontinuebutton(false);
+    setordersummary(true);
   };
 
   const Placeorderbutton = () => {
@@ -27,8 +34,22 @@ const Cartitems = () => {
     }
   };
 
+  const Continuebutton = () => {
+    if (continuebutton == true) {
+      return (
+        <div className="btnactive">
+          <button className="placeorder" onClick={continueorder}>
+            PLACE ORDER
+          </button>
+        </div>
+      );
+    } else {
+      return <div></div>;
+    }
+  };
+
   const Customerdetails = () => {
-    if (ordersummary == false) {
+    if (custdetails == false) {
       return (
         <div className="outercustdets">
           <div className="innercustdets">
@@ -37,12 +58,66 @@ const Cartitems = () => {
         </div>
       );
     } else {
-      return <div className="custdets">content2</div>;
+      return (
+        <div className="outercustdets">
+          <div className="innercustdets">
+            <div className="custdets1">
+              <p style={{ margin: "0%", fontWeight: "600", fontSize: "20px" }}>
+                Customer Details
+              </p>
+              <div className="detsform">
+                <div className="detsfields">
+                  <TextField id="tf" label="Name" variant="outlined" />
+                  <TextField id="tf" label="Phone Number" variant="outlined" />
+                </div>
+                <div className="detsfields">
+                  <TextField id="tf" label="Pincode" variant="outlined" />
+                  <TextField id="tf" label="Locality" variant="outlined" />
+                </div>
+                <div className="addressfield">
+                  <TextField
+                    id="outlined-basic"
+                    label="Address"
+                    variant="outlined"
+                    multiline
+                    rows={2}
+                    fullWidth
+                  />
+                </div>
+                <div className="detsfields">
+                  <TextField id="tf" label="city/town" variant="outlined" />
+                  <TextField id="tf" label="Landmark" variant="outlined" />
+                </div>
+              </div>
+              <div>
+                <p>Type</p>
+                <div>
+                  <form className="radiobuttons">
+                    <label>
+                      <input type="radio" checked="unchecked" name="radio" />
+                      <span>Home</span>
+                    </label>
+                    <label>
+                      <input type="radio" checked="checked" name="radio" />
+                      <span>Work</span>
+                    </label>
+                    <label>
+                      <input type="radio" checked="unchecked" name="radio" />
+                      <span>Other</span>
+                    </label>
+                  </form>
+                </div>
+              </div>
+              <Continuebutton />
+            </div>
+          </div>
+        </div>
+      );
     }
   };
 
   const Ordersummary = () => {
-    if (custdetails == false) {
+    if (ordersummary == false) {
       return (
         <div className="outercustdets">
           <div className="innercustdets">
@@ -51,7 +126,33 @@ const Cartitems = () => {
         </div>
       );
     } else {
-      return <div className="custdets">content2</div>;
+      return (
+        <div className="outercustdets">
+          <div className="innercustdets">
+            <div
+              className="custdets1"
+              style={{ margin: "0%", fontWeight: "600", fontSize: "20px" }}
+            >
+              <p>Order Summary</p>
+              <div className="book">
+                <div>
+                  <img src={book1} alt="" />
+                </div>
+                <div className="bookcon">
+                  <p className="namebook">Book name</p>
+                  <p className="authbook">author</p>
+                  <p className="pricebook">price</p>
+                </div>
+              </div>
+              <div className="btnactive">
+                <button className="placeorder" onClick={continueorder}>
+                  CHECKOUT
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
     }
   };
 
